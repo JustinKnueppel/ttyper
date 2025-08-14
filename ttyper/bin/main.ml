831e0ead39b091674ec1ee862d76b539e5d19cc6
Justin Knueppel <justinknueppel@gmail.com>
2025-08-14 04:19:56 -0400

    Start to work on data model

--- ttyper/bin/main.ml
+++ ttyper/bin/main.ml
@@ -1,9 +1,11 @@
 open Minttea
 
+type screen = Init | Test | End | Quit
+
 type state = {
   words : string list;
   typed_rev : string list; (* *)
-  stop : bool;
+  screen : screen
 }
 
 let rec join sep = function
@@ -11,24 +13,36 @@ let rec join sep = function
   | [s] -> s
   | s::tl -> s ^ sep ^ join sep tl
 
-let init _ = Command.Noop
+let init _ = Command.Enter_alt_screen
 
 let initial_model = {
   words = String.fold_left (fun acc c -> String.make 1 c :: acc) [] "hello world" |> List.rev;
   typed_rev = [];
-  stop = false;
+  screen = Init
 }
+
+let test_end words typed_rev = List.equal String.equal words @@ List.rev typed_rev
 
 let update event model = 
   match event with
-  | Event.KeyDown (Event.Escape, _) -> ({model with stop = true}, Command.Quit)
+  | Event.KeyDown (Event.Enter, _mod) when model.screen = Init -> ({model with screen = Test}, Command.Noop)
+  | Event.KeyDown (Event.Enter, _mod) when model.screen = Test -> ({model with screen = End}, Command.Noop)
+  | Event.KeyDown (Event.Key "q", _mod) when model.screen = End -> ({model with screen = Quit}, Command.Quit)
+  | Event.KeyDown (Event.Escape, _) -> ({model with screen = End}, Command.Exit_alt_screen)
       | Event.KeyDown (Event.Backspace, _mod) -> ({model with typed_rev = List.drop 1 model.typed_rev}, Command.Noop)
-  | Event.KeyDown (Event.Key key, _mod) -> ({model with typed_rev = key :: model.typed_rev }, Command.Noop)
+  | Event.KeyDown (Event.Key key, _mod) -> 
+  let typed_rev = key :: model.typed_rev in
+  let stop = if test_end model.words typed_rev then End else model.screen in
+  let cmd = if stop = End then Command.Exit_alt_screen else Command.Noop in
+    ({model with typed_rev = typed_rev ; screen = stop  }, cmd)
   | Event.KeyDown (Event.Space, _mod) -> ({model with typed_rev = " " :: model.typed_rev }, Command.Noop)
   | _ -> (model, Command.Noop)
 
 let view model =
-  if model.stop then Format.sprintf "Typed %d characters" @@ List.length model.typed_rev
-  else Format.sprintf "%s\n\n%s" (join "" model.words) (join "" @@ List.rev model.typed_rev)
+  match model.screen with
+    | Init -> "Init"
+    | Test -> Format.sprintf "%s\n\n%s" (join "" model.words) (join "" @@ List.rev model.typed_rev)
+    | End -> Format.sprintf "Typed %d characters" @@ List.length model.typed_rev
+    | Quit -> "Quit"
 
 let () = Minttea.app ~init ~update ~view () |> Minttea.start ~initial_model
